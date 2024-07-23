05a3a997050c2016e066eefdcd2d4135697894c3
abdulla <abdullacforu@gmail.com>
2024-07-24 03:58:42 +0530

    change colors and created delivery section

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1582627364"
+    serviceWorkerVersion: "732817068"
   }
 });
